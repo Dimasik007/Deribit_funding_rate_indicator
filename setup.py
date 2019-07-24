9c7c610a993c48e35369b4773a55c2a514e67b66
Dimasik007 <dmitriy00vn@gmail.com>
2019-07-24 21:44:30 +0100

    update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Real time 8h funding rate vs 4h rolling rate indicator for Deribit exchange",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/Dimasik007/Deribit_funding_rate_indicator",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
